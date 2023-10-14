0ba276cd6fd83a254631728ead81f2cadd545c97
Ian Hall <h411.ian@gmail.com>
2023-10-13 19:00:26 -0600

    Initial Commit, project creation

---
+++ DailyDoodle/DailyDoodle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyDoodle
+//
+//  Created by Ian Hall on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyDoodle/DailyDoodle/DailyDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDoodleApp.swift
+//  DailyDoodle
+//
+//  Created by Ian Hall on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
