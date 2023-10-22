ad554073504c40d311c85d7f2e57a541ae471b3c
Ian Hall <h411.ian@gmail.com>
2023-10-22 15:08:14 -0600

    moves drawing and line structs to separate file

--- DailyDoodle/DailyDoodle/UI/Canvas/DrawingView.swift
+++ DailyDoodle/DailyDoodle/UI/Canvas/DrawingView.swift
@@ -44,30 +44,6 @@ struct DrawingView: View {
     }
 }
 
-struct Drawing {
-    var lines: [Line] = []
-    
-    mutating func addPoint(point: CGPoint, color: Color, strokeWidth: Double) {
-        if lines.isEmpty {
-            lines.append(Line(color: color, width: strokeWidth))
-        }
-        lines[lines.count - 1].points.append(point)
-        lines[lines.count - 1].color = color
-        lines[lines.count - 1].width = strokeWidth
-    }
-    
-    mutating func newLine(color: Color, strokeWidth: Double) {
-        lines.append(Line(color: color, width: strokeWidth))
-    }
-}
-
-struct Line: Identifiable {
-    var id = UUID()
-    var color: Color = .white
-    var points: [CGPoint] = []
-    var width: CGFloat
-}
-
 #Preview {
     DrawingView()
 }
