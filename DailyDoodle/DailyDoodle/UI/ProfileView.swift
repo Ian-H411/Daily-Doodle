96db5d6497878cbec4313a36a3f2903c64a84f4b
Ian Hall <h411.ian@gmail.com>
2023-10-24 21:04:08 -0600

    Profile View

---
+++ DailyDoodle/DailyDoodle/UI/ProfileView.swift
@@ -0,0 +1,67 @@
+//
+//  ProfileView.swift
+//  DailyDoodle
+//
+//  Created by Ian Hall on 10/23/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    var body: some View {
+        ScrollView {
+            ProfileHeaderView()
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 3), alignment: .leading, spacing: 0) {
+                ForEach(0..<20, id: \.self) { index in
+                    DrawingDisplayView(drawing: )
+                }
+            }
+        }
+    }
+}
+
+private struct ProfileHeaderView: View {
+    var body: some View {
+        HStack {
+            Image("")
+            VStack {
+                Button("add friend") {
+                    // add friend button here
+                }
+                Text("UserName here")
+                Text("quirky profile desc here")
+            }
+        }
+    }
+}
+
+struct DrawingDisplayView: View {
+    
+    var drawing: Drawing
+    
+    var body: some View {
+        GeometryReader { geometry in
+             ZStack {
+                 ForEach(drawing.lines) { line in
+                     Path { path in
+                         for point in line.points {
+                             let x = point.x * geometry.size.width
+                             let y = point.y * geometry.size.height
+                             if path.isEmpty {
+                                 path.move(to: CGPoint(x: x, y: y))
+                             } else {
+                                 path.addLine(to: CGPoint(x: x, y: y))
+                             }
+                         }
+                     }
+                     .stroke(line.color, lineWidth: line.width)
+                 }
+             }
+         }
+    }
+}
+
+#Preview(body: {
+    ProfileView()
+})
